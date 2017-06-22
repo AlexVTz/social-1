47c99fed23c3b47e211f29e56831fd703fb9274e
Samuel Mercado <samuel.mg@gmx.com>
2017-06-22 17:20:22 -0500

    Crea controlador, modelo y vistas para listar y agregar carreras

--- app/Http/Controllers/CarreraController.php
+++ app/Http/Controllers/CarreraController.php
@@ -14,7 +14,9 @@ class CarreraController extends Controller
      */
     public function index()
     {
-        //
+        $carreras = Carrera::all();
+
+        return view('carrera.indexCarrera', compact('carreras'));
     }
 
     /**
@@ -24,7 +26,7 @@ class CarreraController extends Controller
      */
     public function create()
     {
-        //
+        return view('carrera.formCarrera');
     }
 
     /**
@@ -35,7 +37,9 @@ class CarreraController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Carrera::create($request->input());
+
+        return redirect()->route('carrera.index');
     }
 
     /**
